fbc7a9168b6adbc1e186d8b5213a36930f498426
Alexander Ploner <alex@neatnet.de>
2015-12-10 23:31:24 +0100

    Added accidentally removed DOM query.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function addListener(lWindow) {
     let window = viewFor(lWindow);
 
     window.addEventListener('fullscreen', function () {
+        let toolbox = window.document.getElementById('navigator-toolbox');
         toolbox.style.height = window.fullScreen ? '0px' : 'auto';
         toolbox.style.overflow = window.fullScreen ? 'hidden' : 'auto';
     }, false);
